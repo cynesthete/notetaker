1ed6185dbf60300bb42894cac36cb7855397b126
Tony Rivas <tonyjrivas@gmail.com>
2020-10-10 14:21:19 -0600

    tranfer to main

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static("public"))
+app.use(require("./routes/apiroutes"));
+app.use(require("./routes/htmlroutes"));
+
+app.listen(PORT, function(){
+    console.log(`listening on port ${PORT}`);
+});
